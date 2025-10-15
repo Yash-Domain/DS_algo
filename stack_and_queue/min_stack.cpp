a0496f8c8c4168f3877873e9816b3edf95269bd7
Yash-Domain <yashpawde10@gmail.com>
2025-10-15 11:53:32 +0530

    added min stack

---
+++ stack_and_queue/min_stack.cpp
@@ -0,0 +1,39 @@
+class MinStack {
+public:
+
+    stack<pair<int,int>> st;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty()){
+            st.push({val,val});
+        }
+        int mini = min(val,st.top())
+        st.push({val,mini};)
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        auto i = st.top();
+        return i.first;
+    }
+    
+    int getMin() {
+        auto i = st.top();
+        return i.second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
