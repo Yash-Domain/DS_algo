8c856812ce60379351b00ed3c07503710a37b0f8
Yash-Domain <yashpawde10@gmail.com>
2025-10-17 19:39:06 +0530

    change

--- stack_and_queue/trapping_rainwater.cpp
+++ stack_and_queue/trapping_rainwater.cpp
@@ -26,3 +26,27 @@ public:
 };
 
 // optimal solution 
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int l = 0;
+        int r = n-1 ;
+        long long ans = 0;
+        int lmax = 0;
+        int rmax = 0;
+        while(l<r){
+             lmax = max(lmax,height[l]);
+             rmax = max(rmax,height[r]);
+            if(lmax < rmax){
+                ans += lmax - height[l]; 
+                l++;           
+            }
+            else{
+                ans += rmax - height[r];
+                r--;
+            }
+        }
+        return int(ans);
+    }
+};
