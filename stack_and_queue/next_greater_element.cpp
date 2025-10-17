0ded420d004380fd368c76c4747a074775cb2b85
Yash-Domain <yashpawde10@gmail.com>
2025-10-17 10:28:57 +0530

    changed now

--- stack_and_queue/next_greater_element.cpp
+++ stack_and_queue/next_greater_element.cpp
@@ -1,4 +1,4 @@
-// Brute force 
+// Brute force  O(N*N)
 
 class Solution {
 public:
@@ -21,5 +21,28 @@ public:
             ans.push_back(max_ele);
         }
         return ans; 
+    }
+};
+
+// optimal using stack // O(N)
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        unordered_map<int,int> mp;
+        stack<int> st;
+
+        for(int i = nums2.size() -1 ; i>=0 ; i--){
+            while(!st.empty() && nums2[i] >= st.top()){
+                st.pop();
+            }
+            if(st.empty()) mp[nums2[i]] = -1 ;
+            else mp[nums2[i]] = st.top();
+            st.push(nums2[i]);
+        }
+        for(int i = 0 ; i < nums1.size() ; i++){
+            ans.push_back(mp[nums1[i]]);
+        }
+        return ans;
     }
 };
